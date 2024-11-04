47082f2441f73f529509efca9bce49e808f4ce38
Yuliia Chebarova <yuliia@Yuliias-MacBook-Air.local>
2024-11-04 15:46:05 -0800

    Draft of the to_do app

---
+++ lib/home_page.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_to_do_list/util/todo_tile.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  //list of todo tasks
+  List toDoList = [
+    ["Make Tutorial", false],
+    ["Do workout", false]
+  ];
+
+  void checkBoxChanged(bool? value, int index) {
+    setState(() {
+      toDoList[index][1] = !toDoList[index][1];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.yellow[200],
+      appBar: AppBar(
+        backgroundColor: Colors.yellow[600],
+        title: Text('TO DO'),
+      ),
+      body: ListView.builder(
+        itemCount: toDoList.length,
+        itemBuilder: (context, index) {
+          return TodoTile(
+              taskName: toDoList[index][0],
+              taskCompleted: toDoList[index][1],
+              onChanged: (value) => checkBoxChanged(value, index));
+        },
+      ),
+    );
+  }
+}
